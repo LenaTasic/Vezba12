799679e425b2af600a1042c7a0813281876a08bb
LenaTasic <97707992+LenaTasic@users.noreply.github.com>
2023-12-13 09:00:23 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Card card = new Card(500);
+
+        card.charge(500);
+
+        card.charge(100);
+
+    }
+}
